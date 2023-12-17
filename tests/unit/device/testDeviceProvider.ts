f478215b1c6c058c234d7d931ba2f12b9a14a27e
heavyrubberslave <pascal@heavyrubberslave.com>
2023-12-17 20:52:52 +0100

    Code-style fix

--- tests/unit/device/testDeviceProvider.ts
+++ tests/unit/device/testDeviceProvider.ts
@@ -3,9 +3,8 @@ import DeviceProvider from "../../../src/device/provider/deviceProvider.js";
 
 export default class TestDeviceProvider extends DeviceProvider
 {
-    public constructor(
-        eventEmitter: EventEmitter
-    ) {
+    public constructor(eventEmitter: EventEmitter)
+    {
         super(eventEmitter);
     }
 
