3e3b5944d6e89c18c9520409f22898897ea6b852
HRS <pascal@heavyrubberslave.com>
2023-12-29 15:11:24 +0100

    Fix tests

--- tests/unit/device/testDeviceProvider.ts
+++ tests/unit/device/testDeviceProvider.ts
@@ -8,8 +8,9 @@ export default class TestDeviceProvider extends DeviceProvider
         super(eventEmitter);
     }
 
-    public init(): void
+    public init(): Promise<void>
     {
         // noop
+        return new Promise<void>((resolve) => resolve());
     }
 }
