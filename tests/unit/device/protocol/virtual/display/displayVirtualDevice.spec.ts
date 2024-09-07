6d55942bf61e4552728c351f82437c66c0d18f55
heavyrubberslave <pascal@heavyrubberslave.com>
2024-09-07 11:51:36 +0200

    Add a test

---
+++ tests/unit/device/protocol/virtual/display/displayVirtualDevice.spec.ts
@@ -0,0 +1,37 @@
+import DisplayVirtualDevice from "../../../../../../src/device/protocol/virtual/display/displayVirtualDevice.js";
+
+describe('DisplayVirtualDevice', () => {
+
+    function createDevice(): DisplayVirtualDevice {
+        return new DisplayVirtualDevice(
+            '1.0.0',
+            'device-id',
+            'device name',
+            'device model',
+            'buttplugIo',
+            new Date(),
+            {},
+        );
+    }
+
+    it('it returns content that has been set', async () => {
+
+        // Arrange
+        const device = createDevice();
+
+        const content = 'hello world';
+
+        // Act
+        await device.setAttribute('content', content);
+        const result = expect(device.getAttribute('content'));
+
+        // Assert
+        result.toBe(content);
+    });
+
+    it('it has a refresh rate of 175ms', async () => {
+        const device = createDevice();
+
+        expect(device.getRefreshInterval).toBe(175);
+    });
+});
