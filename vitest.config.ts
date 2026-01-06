0bff4d3c24ea515c010b1096b3b8eb501b3cc59d
HRS <pascal@heavyrubberslave.com>
2026-01-06 18:48:10 +0100

    Fix coverage collection

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,23 @@ export default defineConfig({
   test: {
     environment: "node",
     setupFiles: ["./tests/vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      include: [
+        "src/**/*.ts",
+        "src/**/*.js",
+      ],
+      thresholds: {
+        global: {
+          branches: 0,
+          functions: 0,
+          lines: 0,
+          statements: 0,
+        },
+        perFile: true,
+      },
+    },
   },
 });
