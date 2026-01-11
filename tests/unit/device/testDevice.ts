cdf908f5d1d36b548262fb930dc1e70fc8aef753
HRS <pascal@heavyrubberslave.com>
2026-01-11 11:35:41 +0100

    Fix error for testDevice

--- tests/unit/device/testDevice.ts
+++ tests/unit/device/testDevice.ts
@@ -8,7 +8,7 @@ export default class TestDevice extends Device
         connectedSince: Date,
         controllable: boolean
     ) {
-        super(deviceId, deviceName, 'dummy', connectedSince, controllable, {});
+        super(deviceId, deviceName, 'dummy', connectedSince, controllable, {}, {});
     }
 
     public refreshData(): Promise<void>
