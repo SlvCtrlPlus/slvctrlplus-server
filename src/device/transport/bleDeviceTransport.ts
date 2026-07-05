5e22d8772010c3aca7f5642d37600b9e34a7da7c
HRS <pascal@heavyrubberslave.com>
2026-07-05 11:05:14 +0200

    Fix stale class name reference in comment

--- src/device/transport/bleDeviceTransport.ts
+++ src/device/transport/bleDeviceTransport.ts
@@ -60,7 +60,7 @@ export default class BleUartDeviceTransport implements DeviceBidirectionalTransp
             }
 
             const { characteristics } = await this.peripheral.discoverSomeServicesAndCharacteristicsAsync(
-                [/* AiroticDeviceProvider.UART_SERVICE_UUID */],
+                [/* AiroticDeviceFactory.UART_SERVICE_UUID */],
                 [this.uartRxCharacteristicUuid, this.uartTxCharacteristicUuid],
             );
 
